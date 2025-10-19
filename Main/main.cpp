8a0ad404ac48e989cf907f52f3e8224881f10b36
Mateusz533 <mateuszfrejlich533@gmail.com>
2025-10-19 19:57:23 +0200

    add tests for CUDA stream usage

--- Main/main.cpp
+++ Main/main.cpp
@@ -10,7 +10,7 @@
 
 template<size_t N, typename Fun, typename... Args>
 inline void runPerformanceTest(const std::string& name, Fun&& fun, Args&... args) {
-	std::cout << "Test of calling `" << name << "`:" << std::endl;
+	std::cout << "Test of calling synchronous function \033[34m`" << name << "`\033[0m:" << std::endl;
 	{
 		using ResultType = std::invoke_result_t<Fun, Args&...>;
 		auto callable = std::forward<Fun>(fun);
@@ -41,6 +41,54 @@ inline void runPerformanceTest(const std::string& name, Fun&& fun, Args&... args
 	std::cout << std::endl;
 }
 
+template<size_t N, typename Fun, typename... Args>
+inline void runPerformanceTestWithStream(const std::string& name, Fun&& fun, Args&... args) {
+	std::cout << "Test of calling asynchronous function \033[32m`" << name << "`\033[0m:" << std::endl;
+	{
+		using ResultType = std::invoke_result_t<Fun, Args&..., cv::cuda::Stream&>;
+		auto callable = std::forward<Fun>(fun);
+
+		cv::cuda::Stream stream;
+		cv::cuda::Event startEvent{cv::cuda::Event::CreateFlags::BLOCKING_SYNC};
+		cv::cuda::Event endEvent{cv::cuda::Event::CreateFlags::BLOCKING_SYNC};
+
+		// Do not measure initialization time
+		std::invoke(callable, args..., stream);
+		stream.waitForCompletion();
+
+		const auto startTime = std::chrono::high_resolution_clock::now();
+		startEvent.record(stream);
+
+		for(int i = 0; i < N; ++i) {
+			std::invoke(callable, args..., stream);
+		}
+
+		const auto asyncEndTime = std::chrono::high_resolution_clock::now();
+		endEvent.record(stream);
+
+		stream.waitForCompletion();
+		const auto syncEndTime = std::chrono::high_resolution_clock::now();
+
+		const auto totalAsyncTimeUs = std::chrono::duration<double, std::micro>(asyncEndTime - startTime).count();
+		const auto totalChronoTimeUs = std::chrono::duration<double, std::micro>(syncEndTime - startTime).count();
+		const auto totalEventsTimeUs = cv::cuda::Event::elapsedTime(startEvent, endEvent) * 1000.0;
+
+		printf("- One asynchronous call period : %8.3f us\n", totalAsyncTimeUs / N);
+		printf("- One operation period (chrono): %8.3f us\n", totalChronoTimeUs / N);
+		printf("- One operation period (events): %8.3f us\n", totalEventsTimeUs / N);
+
+		if constexpr(!std::is_void_v<ResultType>) {
+			const auto lastResult = std::invoke(callable, args..., stream);
+
+			std::cout << "- Last result: ";
+			if constexpr(std::is_same_v<ResultType, bool>)
+				std::cout << std::boolalpha;
+			std::cout << lastResult << std::endl;
+		}
+	}
+	std::cout << std::endl;
+}
+
 int main() {
 	constexpr size_t N = 1'000;
 
@@ -57,9 +105,15 @@ int main() {
 	/* ======================================================================================================================== */
 
 	runPerformanceTest<N>("processImageWithCuda (BGR image)", processImageWithCuda, bgrImg, tempDest);
+	runPerformanceTestWithStream<N>("processImageWithCudaAsync (BGR image)", processImageWithCudaAsync, bgrImg, tempDest);
 
-	constexpr auto copyTo = (void(cv::cuda::GpuMat::*)(cv::cuda::GpuMat&) const)(&cv::cuda::GpuMat::copyTo);
-	runPerformanceTest<N>("copyTo (GRAY image)", copyTo, grayImg, tempDest);
-	runPerformanceTest<N>("copyTo (BGR image)", copyTo, bgrImg, tempDest);
-	runPerformanceTest<N>("copyTo (BGRA image)", copyTo, bgraImg, tempDest);
+	constexpr auto syncCopyTo = (void(cv::cuda::GpuMat::*)(cv::cuda::GpuMat&) const)(&cv::cuda::GpuMat::copyTo);
+	runPerformanceTest<N>("copyTo (GRAY image)", syncCopyTo, grayImg, tempDest);
+	runPerformanceTest<N>("copyTo (BGR  image)", syncCopyTo, bgrImg, tempDest);
+	runPerformanceTest<N>("copyTo (BGRA image)", syncCopyTo, bgraImg, tempDest);
+
+	constexpr auto asyncCopyTo = (void(cv::cuda::GpuMat::*)(cv::cuda::GpuMat&, cv::cuda::Stream&) const)(&cv::cuda::GpuMat::copyTo);
+	runPerformanceTestWithStream<N>("copyTo (GRAY image)", asyncCopyTo, grayImg, tempDest);
+	runPerformanceTestWithStream<N>("copyTo (BGR  image)", asyncCopyTo, bgrImg, tempDest);
+	runPerformanceTestWithStream<N>("copyTo (BGRA image)", asyncCopyTo, bgraImg, tempDest);
 }
